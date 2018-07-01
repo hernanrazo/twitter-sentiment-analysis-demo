486ca22125d2e834b126d10249508b2a740588c4
Hernan Razo <hernanrazo@gmail.com>
2018-06-30 19:24:52 -0500

    Added comments to code.

--- sentiment_demo.py
+++ sentiment_demo.py
@@ -1,18 +1,22 @@
 import tweepy
 from textblob import TextBlob
 
+#Enter Twitter API credentials
 consumer_key = 'YOUR KEY HERE'
 consumer_secret = 'YOUR SECRET HERE'
 access_token = 'YOUR TOKEN HERE'
 access_token_secret = 'YOUR TOKEN SECRET HERE'
 
+#Use Twitter credentials to access tweets
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
+#Set topic you want to search for. In this case, Twenty One Pilots
 public_tweets = api.search('twenty one pilots')
 
+#Iterate through public tweets, print them, and 
+#return the polarity and subjectiity values
 for tweet in public_tweets:
 	print(tweet.text)
 	analysis = TextBlob(tweet.text)
